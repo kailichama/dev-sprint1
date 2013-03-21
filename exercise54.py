24b0a10b128a323441fc7ae81058a02a9d53e2a6
kailichama <chiang.kellymimi@gmail.com>
2013-03-21 16:15:42 -0400

    kelly's triangle

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,16 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Kelly Chiang
+
+def is_triangle(a, b, c):
+    if a + b < c:
+        print "no"
+    if a + c < b:
+        print "no"
+    if b + c < a:
+        print "no"
+    else:
+        print "yes"
+is_triangle(29, 11, 12)
+
+is_triangle(10, 11, 12)
+
