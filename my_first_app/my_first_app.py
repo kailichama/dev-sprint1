b451d1de383127f45e4c9bd416873719bd043e62
kailichama <chiang.kellymimi@gmail.com>
2013-05-01 11:37:10 -0400

    my_first_app.py
    code plus notes

--- my_first_app/my_first_app.py
+++ my_first_app/my_first_app.py
@@ -6,12 +6,15 @@ app = flask.Flask(__name__)
 
 class View(flask.views.MethodView):
 	def get(self):   
-		return flask.render_template('index.html')
+		return flask.render_template('index.html') #flask defaults to a "templates" folder, remember to create a templates folder!
 
-	def post(self):
-		return "Works!"
+	def post(self): #when you have a get, should following with post if you want something to be displayed
+		result = eval(flask.request.form['expression']) #links to the HTML form where the user types into the textbox named "expression"
+		flask.flash(result)
+		return self.get()
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST']) #lets the app know youre using more views, methods is a list
 
 app.debug = True
 app.run()
+
